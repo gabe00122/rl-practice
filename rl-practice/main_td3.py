fcab76aa20d4443b8b51f46eddcd4360a180c650
Gabriel keith <gabe.keith@yahoo.com>
2023-09-07 07:31:39 -0400

    init

---
+++ rl-practice/main_td3.py
@@ -0,0 +1,104 @@
+import gymnasium as gym
+
+import torch
+import torch.nn as nn
+
+# import the skrl components to build the RL system
+from skrl.agents.torch.td3 import TD3, TD3_DEFAULT_CONFIG
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, Model
+from skrl.resources.noises.torch import GaussianNoise
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
+
+
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class Actor(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False):
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, self.num_actions))
+
+    def compute(self, inputs, role):
+        return torch.tanh(self.net(inputs["states"])), {}
+
+class Critic(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False):
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations + self.num_actions, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 1))
+
+    def compute(self, inputs, role):
+        return self.net(torch.cat([inputs["states"], inputs["taken_actions"]], dim=1)), {}
+
+
+# load and wrap the gymnasium environment.
+# note: the environment version may change depending on the gymnasium version
+try:
+    env = gym.make("BipedalWalker-v3", hardcore=False, render_mode="rgb_array")
+    env = gym.wrappers.RecordVideo(env, "videos/torch/BipedalWalkerTD3-v2", lambda episode: episode % 100 == 0)
+except (gym.error.DeprecatedEnv, gym.error.VersionNotFound) as e:
+    print("The environment is not available. Please install the latest version of gymnasium.")
+env = wrap_env(env)
+
+device = env.device
+
+memory = RandomMemory(memory_size=200000, num_envs=env.num_envs, device=device, replacement=False)
+
+
+models = {}
+models["policy"] = Actor(env.observation_space, env.action_space, device)
+models["target_policy"] = Actor(env.observation_space, env.action_space, device)
+models["critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["critic_2"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
+
+for model in models.values():
+    model.init_parameters(method_name="normal_", mean=0.0, std=0.1)
+
+
+cfg = TD3_DEFAULT_CONFIG.copy()
+cfg["exploration"]["noise"] = GaussianNoise(0, 0.1, device=device)
+cfg["smooth_regularization_noise"] = GaussianNoise(0, 0.2, device=device)
+cfg["smooth_regularization_clip"] = 0.5
+cfg["discount_factor"] = 0.99
+cfg["learning_starts"] = 25000
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 75
+cfg["experiment"]["checkpoint_interval"] = 750
+cfg["experiment"]["directory"] = "runs/torch/BipedalWalkerTD3"
+
+agent = TD3(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 5000000, "headless": True}
+
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[agent])
+
+# start training
+trainer.train()
